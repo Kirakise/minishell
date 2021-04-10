9d91c50e59f4e293300a56a1f155ff06ddf59af9
Reilly Caraway <rcaraway@ga-s1.msk.21-school.ru>
2021-04-10 18:17:56 +0300

    no more /bin/

--- srcs/foncs/execve.c
+++ srcs/foncs/execve.c
@@ -7,7 +7,16 @@ extern t_shell g_shell;
 
 int do_exec(t_cmd *cmd)
 {
-	execve(cmd->exec_name, cmd->args, g_shell.env);
+	char *s;
+	char *s2;
+
+	s = ft_strjoin("/bin/", cmd->exec_name);
+	s2 = ft_strjoin("/usr/bin/", cmd->exec_name);
+	if (open(cmd->exec_name, O_RDONLY) != -1)
+		execve(cmd->exec_name, cmd->args, g_shell.env);
+	if (open(s, O_RDONLY) != -1)
+		execve(s, cmd->args, g_shell.env);
+	execve(s2, cmd->args, g_shell.env);
 	char *s1 = strerror(errno);
 	printf("%s\n", s1);
 	exit(-1);
