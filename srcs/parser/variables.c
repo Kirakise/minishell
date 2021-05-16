b74561f652ff14bd61d75b94ca6f286f9f26bafd
rgreater <rgreater@student.21-school.ru>
2021-05-17 02:32:35 +0300

    norme (except for the comments)

--- srcs/parser/variables.c
+++ srcs/parser/variables.c
@@ -1,47 +1,59 @@
 #include "../../includes/minishell.h"
 
-void	subst_vars(char **str)
+static char	*get_var_value(char *s, int i)
 {
-	char	*s;
-	char	*start;
 	char	*var_name;
 	char	*var_value;
-	int		i;
-	char	*res;
-	char	*tmp;
 
-	if (!str || !*str)
-		return ;
-	s = *str;
-	res = ft_strdup("");//malloc
-	tmp = 0;
+	var_name = ft_substr(s + 1, 0, i);//malloc
+	var_value = find_var(var_name);//malloc
+	free(var_name);
+	return (var_value);
+}
+
+static void	str_iterate(char *s, char **res)
+{
+	char	*start;
+	char	*var_value;
+	char	*tmp;
+	int		i;
+
 	while (*s)
 	{
 		start = s;
 		while (*s && *s != '$')
 			s++;
 		if (!*s)
-			break ;
-		//we should be in '$' now...
-		i = 0;
+			return ;
+		i = 0;//we should be in '$' now...
 		while (s[i + 1] && !ft_isspace(s[i + 1]))
 			i++;
-		var_name = ft_substr(s + 1, 0, i);//malloc
-		var_value = find_var(var_name);//malloc
-		free(var_name);
+		var_value = get_var_value(s, i);
 		*s = 0;// 0 instead of $
-		tmp = ft_strjoin_3(res, start, var_value);//malloc
-		free(res);
+		tmp = ft_strjoin_3(*res, start, var_value);//malloc
+		free(*res);
 		free(var_value);
-		res = tmp;
+		*res = tmp;
 		tmp = 0;
 		s += i + 1;
 	}
-	if (!*res)
-	{
-		free(res);
+}
+
+void	subst_vars(char **str)
+{
+	char	*s;
+	char	*res;
+
+	if (!str || !*str)
 		return ;
+	s = *str;
+	res = ft_strdup("");//malloc
+	str_iterate(s, &res);
+	if (!*res)
+		free(res);
+	else
+	{
+		free(*str);
+		*str = res;
 	}
-	free(*str);
-	*str = res;
 }
