efb76f9477f03b87b024079dba70e3c266cd969f
rgreater <rgreater@student.21-school.ru>
2021-05-16 17:48:06 +0300

    few adjusments

--- srcs/temp/tmpread.c
+++ srcs/temp/tmpread.c
@@ -24,61 +24,60 @@ int delete_last(char *s)
 	return (1);
 }
 
+static void access_history(char *str, char **s, int *end)
+{
+	tputs(restore_cursor, g_shell.tmp_fd_1, ft_putchar);
+	tputs(tigetstr("ed"), 1, ft_putchar);
+	free(*s);
+	if (!ft_strcmp(str, "\e[A"))
+		*s = get_history_line(&g_shell.hist, 1, end);
+	else
+		*s = get_history_line(&g_shell.hist, -1, end);
+	if (*s)
+		ft_putstr(*s);
+		//write(g_shell.tmp_fd_1, s, ft_strlen(s));
+}
+
 char *tmpread()
 {
 	int l;
-	int end = 0;
+	int end;
 	char *str;
 	char *s;
 	char *tmp;
 
 	s = ft_calloc(1, 1);
-	str = 0;
+	str = ft_calloc(1, 1);
+	end = 0;
 	tputs(save_cursor, 1, ft_putchar);
-	do
-		{
-			free(str);
+	while (ft_strcmp(str, "\n"))
+	{
+		free(str);
+		str = ft_calloc(5, 1);
+		l = read(g_shell.tmp_fd_0, str, 10);
+		if (!ft_strcmp(str, "\e[A") || !ft_strcmp(str, "\e[B"))
+			access_history(str, &s, &end);
+		else if (!ft_strcmp(str, "\e[C") || !ft_strcmp(str, "\e[D"))
 			tmp = 0;
-			str = ft_calloc(5, 1);
-			l = read(g_shell.tmp_fd_0, str, 10);
-			if (!ft_strcmp(str, "\e[A") || !ft_strcmp(str, "\e[B"))
-			{
-				tputs(restore_cursor, g_shell.tmp_fd_1, ft_putchar);
-				tputs(tigetstr("ed"), 1, ft_putchar);
-				free(s);
-				if (!ft_strcmp(str, "\e[A"))
-					s = get_history_line(&g_shell.hist, 1, &end);
-				else
-					s = get_history_line(&g_shell.hist, -1, &end);
-				if (s)
-				{
-					ft_putstr(s);
-					//write(g_shell.tmp_fd_1, s, ft_strlen(s));
-				}
-			}
-			else if (!ft_strcmp(str, "\e[C") || !ft_strcmp(str, "\e[D"))
-				tmp = 0;
-			else if (!ft_strcmp(str, "\x7f"))
-			{
-				if (delete_last(s) == 1)
-				{
-					tputs(cursor_left, 1, ft_putchar);
-					tputs(delete_character, 1, ft_putchar);
-				}
-			}
-			else if (!ft_strcmp(str, "\3"))
-			{
-				free(str);
-				break ;
-			}
-			else if (!ft_strcmp(str, "\4") && s[0] == 0)
-				exit (0);
-			else if (str[0] >= 20 && str[0] <= 126)
-			{
-				write(1, str, l);
-				ft_realloc(&s, str);
-			}
-		} while (ft_strcmp(str, "\n"));
+		else if (!ft_strcmp(str, "\x7f") && delete_last(s) == 1)
+		{
+			tputs(cursor_left, 1, ft_putchar);
+			tputs(delete_character, 1, ft_putchar);
+		}
+		else if (!ft_strcmp(str, "\3"))
+			break ;
+		else if (!ft_strcmp(str, "\4") && !*s)
+		{
+			s = ft_strdup("exit");
+			ft_putstr(s);
+			break ;
+		}
+		else if (str[0] >= 20 && str[0] <= 126)
+		{
+			write(1, str, l);
+			ft_realloc(&s, str);
+		}
+	}
 	free(str);
 	write(1, "\n", 1);
 	return (s);
