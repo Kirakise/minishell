84e8cff5ebb17593e092a24638b8c724154cb9fd
rgreater <rgreater@student.21-school.ru>
2021-05-20 01:52:46 +0300

    fixed echo not writing newline without args

--- srcs/foncs/echo.c
+++ srcs/foncs/echo.c
@@ -9,14 +9,13 @@ void	ft_echo(t_cmd *cmd)
 	i = 1;
 	if (cmd->args[i] && !ft_strcmp(cmd->args[i], "-n"))
 		i++;
-	while (cmd->args[i] && cmd->args[i + 1])
+	while (cmd->args[i])
 	{
 		write(g_shell.fd_1, cmd->args[i], ft_strlen(cmd->args[i]));
-		write(1, " ", 1);
 		i++;
+		if (cmd->args[i])
+			write(1, " ", 1);
 	}
-	if (cmd->args[i])
-		write(g_shell.fd_1, cmd->args[i], ft_strlen(cmd->args[i]));
 	if (!cmd->args[1] || (cmd->args[1] && ft_strcmp(cmd->args[1], "-n")))
 		write(g_shell.fd_1, "\n", 1);
 	exit(0);
