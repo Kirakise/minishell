907f3474f4ba015747a4a5014185b3970878e873
rgreater <rgreater@student.21-school.ru>
2021-05-19 23:26:07 +0300

    added $?

--- includes/libft.h
+++ includes/libft.h
@@ -36,6 +36,7 @@ char				*ft_strjoin_3(char const *s1, char const *s2,
 char				*ft_strjoin_space(char const *s1, char const *s2);
 char				**ft_split(char const *str, char c);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
+char				*ft_itoa(int n);
 
 /* Lists */
 t_list				*ft_lstnew(void *content);
--- srcs/parser/variables.c
+++ srcs/parser/variables.c
@@ -1,13 +1,20 @@
 #include "../../includes/minishell.h"
 
-static char	*get_var_value(char *s, int i)
+extern t_shell	g_shell;
+
+static char	*get_var_value(char *s, int *i)
 {
 	char	*name;
 	char	*value;
 
-	if (i == 0)
+	if (*i == 0 && s[*i] == '?')
+	{
+		*i += 1;
+		return (ft_itoa(g_shell.status));//malloc
+	}
+	if (*i == 0)
 		return (ft_strdup("$"));
-	name = ft_substr(s, 0, i);//malloc
+	name = ft_substr(s, 0, *i);//malloc
 	value = find_var(name);//malloc
 	free(name);
 	return (value);
@@ -23,28 +30,25 @@ static void	str_iterate(char *s, char **res)
 	while (*s)
 	{
 		start = s;
-//		printf("start = %s\n", start);
 		while (*s && *s != '$')
 			s++;
-		if (!*s)
-		{
-			tmp = ft_strjoin(*res, start);//malloc
-			free(*res);
-			*res = tmp;
-			return ;
-		}
 		while (*s == '$')
 			s++;
-		i = 0;
-		while (ft_isalnum(s[i]))
-			i++;
-		var = get_var_value(s, i);
-		*(s - 1) = 0;// 0 instead of $
-		tmp = ft_strjoin_3(*res, start, var);//malloc
+		if (!*s)
+			tmp = ft_strjoin(*res, start);//malloc
+		else
+		{
+			i = 0;
+			while (ft_isalnum(s[i]))
+				i++;
+			var = get_var_value(s, &i);//malloc
+			*(s - 1) = 0;// 0 instead of $
+			tmp = ft_strjoin_3(*res, start, var);//malloc
+			free(var);
+			s += i;
+		}
 		free(*res);
-		free(var);
 		*res = tmp;
-		s += i;
 	}
 }
 
