df4d788204a30a49f2c4368170361441783675ed
rgreater <rgreater@student.21-school.ru>
2021-05-21 23:32:34 +0300

    upd strdup - tiny change

--- srcs/libft/src/ft_strdup.c
+++ srcs/libft/src/ft_strdup.c
@@ -8,7 +8,7 @@ char	*ft_strdup(const char *s1)
 
 	tmp2 = (char *)s1;
 	size = ft_strlen(tmp2);
-	ret = malloc(sizeof(char) * size + 1);
+	ret = malloc(sizeof(char) * (size + 1));
 	if (ret == 0)
 		return (0);
 	tmp2 = ret;
