dd890ae499962d6549a31aa52d99d59d32d9ba24
rgreater <rgreater@student.21-school.ru>
2021-05-26 21:37:35 +0300

    Fixed exit status on 'exit' command errors

--- srcs/foncs/exit.c
+++ srcs/foncs/exit.c
@@ -9,7 +9,7 @@ void	sh_exit(t_cmd *cmd)
 	if (cmd->args[1] && cmd->args[2])
 	{
 		ft_putstr_nl("exit: too many arguments");
-		g_shell.status = 1;//потом видимо обнуляется, с этим надо что-то решить
+		g_shell.status = 1;
 	}
 	else if (cmd->args[1])
 	{
@@ -21,7 +21,7 @@ void	sh_exit(t_cmd *cmd)
 			ft_putstr("exit: ");
 			ft_putstr(cmd->args[1]);
 			ft_putstr_nl(": numeric argument required");
-			g_shell.status = 255;//потом видимо обнуляется, с этим надо что-то решить
+			g_shell.status = 255;
 		}
 		else
 			exit(ft_atoi(cmd->args[1]));
