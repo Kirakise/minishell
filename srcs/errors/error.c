66dbd6f1f90e276b10c6764bf7a32cd450242f57
rgreater <rgreater@student.21-school.ru>
2021-06-01 18:26:24 +0300

    mv ft_puterr to cd.c

--- srcs/errors/error.c
+++ srcs/errors/error.c
@@ -7,13 +7,3 @@ void	malloc_err(void)
 	write(g_shell.tmp_fd_1, "malloc error!\n", 14);
 	exit(-30);
 }
-
-void	ft_puterr(char *program_name, char *arg, int status)
-{
-	g_shell.status = status;
-	ft_putstr(program_name);
-	ft_putstr(": ");
-	ft_putstr(arg);
-	ft_putstr(": ");
-	ft_putstr_nl(strerror(errno));
-}
